0b632c39c2658e2c5449deba00507044d6c6a6d7
App Academy Student <student@appacademy.io>
2014-10-22 13:23:50 -0700

    initial commit

---
+++ app/models/short_url.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: short_urls
+#
+#  id           :integer          not null, primary key
+#  long_url     :string(255)
+#  short_url    :string(255)
+#  submitter_id :integer
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class ShortUrl < ActiveRecord::Base
+
+end
